7062a875298c8c4cc55304346b28173dbb9b758d
Duc H. Phan <ptdat.thanhdatphan@gmail.com>
2019-03-12 21:58:49 +0200

    import dependences

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
+          <p>JuzTalent 3.0 - CMS</p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://juztalent.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Visit Us
           </a>
         </header>
       </div>
